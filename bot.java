e7b0ef32620742dde45a56e26360e1fc4c5d24de
AOKKss <44952189+AOKKss@users.noreply.github.com>
2018-11-15 23:11:14 +0100

    Add files via upload

---
+++ bot.java
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+    client.user.setGame(whit your Sister`,'https://www.twitch.tv/Savage89')
+
+});
+client.login("MzU4NjY0OTk5OTE0NDM4NjU4.Dsf33A.RM4w8RJ2IgdRr0tGz9LI8iHPRu4")
